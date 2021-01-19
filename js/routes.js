bca3b68c8ecb2b7f6b67886c39718801104f48c3
Selena Singleton <ssingle7@gmail.com>
2021-01-18 21:28:32 -0500

    modified index js file with functions to get and save data

--- js/routes.js
+++ js/routes.js
@@ -2,40 +2,33 @@ const fs = require('fs');
 const util = require('util');
 const path = require('path');
 
+
 module.exports = app => {
 
-    var readFileAsync = util.promisify(fs.readFile);
-
     // Setup notes variable
-    readFileAsync("db/db.json","utf8").then(function(data) {
-        // console.log("Reading db file");
     fs.readFile("db/db.json","utf8", (err, data) => {
 
         if (err) throw err;
 
+        // Store the contents of the db.json file in a variable for better performance
         var notes = JSON.parse(data);
 
         // API ROUTES
         // ========================================================
-
-        // Read the db.json file and return all saved notes as JSON.
+    
         // Setup the /api/notes get route
         app.get("/api/notes", function(req, res) {
             // Read the db.json file and return all saved notes as JSON.
             res.json(notes);
         });
 
-        // Receives a new note, adds it to db.json, then returns the new note
         // Setup the /api/notes post route
         app.post("/api/notes", function(req, res) {
             // Receives a new note, adds it to db.json, then returns the new note
             let newNote = req.body;
             notes.push(newNote);
-
             updateDb();
-
             return console.log("Added new note: "+newNote.title);
-
         });
 
         // Retrieves a note with specific id
@@ -58,8 +51,7 @@ module.exports = app => {
         app.get('/notes', function(req,res) {
             res.sendFile(path.join(__dirname, "../public/notes.html"));
         });
-
-
+        
         // Display index.html when all other routes are accessed
         app.get('*', function(req,res) {
             res.sendFile(path.join(__dirname, "../public/index.html"));
@@ -68,10 +60,11 @@ module.exports = app => {
         //updates the json file whenever a note is added or deleted
         function updateDb() {
             fs.writeFile("db/db.json",JSON.stringify(notes,'\t'),err => {
-                if (err) return console.log(err);
                 if (err) throw err;
                 return true;
             });
         }
+
     });
-} 
+
+}
